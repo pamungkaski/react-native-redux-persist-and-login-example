e8d829f45ad8a38292b56feb8b13a3a3013df8fa
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-09-21 18:54:31 +0700

    first init

---
+++ src/actions/login.js
@@ -0,0 +1,9 @@
+/**
+ * Created by iampamungkas on 9/21/17.
+ */
+export const FORGOT_PASSWORD = 'FORGOT_PASSWORD'
+export const LOGIN = 'LOGIN'
+export const REGISTER = 'REGISTER'
+export const CHECK_USERNAME = 'CHECK_USERNAME'
+
+

94df529c7b72884d5c87cf06fbe348456d7fa172
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-09-21 19:08:06 +0700

    Add Login actions'

--- src/actions/login.js
+++ src/actions/login.js
@@ -2,7 +2,8 @@
  * Created by iampamungkas on 9/21/17.
  */
 export const FORGOT_PASSWORD = 'FORGOT_PASSWORD'
-export const LOGIN = 'LOGIN'
+export const LOGIN_REQUEST = 'LOGIN_REQUEST'
+export const LOGIN_RECEIVE = 'LOGIN_RECEIVE'
 export const REGISTER = 'REGISTER'
 export const CHECK_USERNAME = 'CHECK_USERNAME'
 

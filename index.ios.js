78d241c7105d59cc685d74fb2414c942e72fb9ae
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-09-27 16:58:20 +0700

    Ios

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    View,
+    AsyncStorage
 } from 'react-native';
+import configureStore from './src/store/configureStore'
+import { Provider } from 'react-redux'
+import HomeNavigation from './src/components/Home/views/HomeNavigation'
+import { persistStore } from 'redux-persist'
 
+const store = configureStore();
+persistStore(store, {storage: AsyncStorage, blacklist:['Home', 'Login']})
 export default class RegEdit extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+              <View style={container1}>
+                <HomeNavigation/>
+              </View>
+            </Provider>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
+const container1 = {
+    backgroundColor: "#ffffff",
+    flex: 1
+};
 AppRegistry.registerComponent('RegEdit', () => RegEdit);
